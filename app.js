2cf4fb1e07b87bc897a051f8e5fcf7cb51a25c1b
gollandl <lydia.golland@britishgas.co.uk>
2017-08-27 16:01:39 +0100

    Installed packages

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -18,13 +12,6 @@ export default class Calibration extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
