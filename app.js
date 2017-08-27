69d026e108365dddc696245ecf3a75b862d24099
gollandl <lydia.golland@britishgas.co.uk>
2017-08-27 17:41:39 +0100

    Header component

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import NavigatorWithState from './src/navigation/NavigatorWithState';
 import store from './src/store';
+import Header from './src/components/header';
 
 export default class App extends Component {
   render() {
     return (
         <Provider store={store}>
+          <View style={{ flex: 1 }}>
+            <Header />
             <NavigatorWithState />
+          </View>
         </Provider>
     );
   }
